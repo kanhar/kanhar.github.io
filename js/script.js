c6b8f6320840350b22fda52f7706361d9ec8a7e2
Kanhar Munshi <kmunshi@microsoft.com>
2021-07-10 19:17:54 -0700

    Fix heaer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
 document.querySelector("#nav-btn").addEventListener("click", () => {
-    
-    //document.querySelector("#main-header").classList.toggle("show");
-
+    document.querySelector("#main-header").classList.toggle("show");
 });
