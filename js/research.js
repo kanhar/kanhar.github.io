16f3090025f3cce88d30d01d3f5ff88db4345987
Kanhar Munshi <kmunshi@microsoft.com>
2021-08-21 19:44:21 -0700

    scribd test

--- js/research.js
+++ js/research.js
@@ -75,7 +75,16 @@ else
             div.innerHTML += `
         ${output}
         <div style="margin: 3.2rem auto;">
-            <iframe width="300" height="300" src="${articles}" name="page"  ></iframe>
+         <p  style=" margin: 12px auto 6px auto; font-family: Helvetica,Arial,Sans-serif; font-style: normal; font-variant: normal; font-weight: normal; font-size: 14px; line-height: normal; font-size-adjust: none; font-stretch: normal; -x-system-font: none; display: block;">   
+            <a title="View 122018_SunilMunshiTeaTrails on Scribd" href="https://img1.wsimg.com/blobby/go/daf47534-09c5-48e5-a358-420f36953360/Tea%20Trails%20Dec%202018.pdf" 
+                 style="text-decoration: underline;" 
+            >122018_SunilMunshiTeaTrails</a> by 
+            <a title="View Kanhar Munshi's profile on Scribd" 
+            href="https://img1.wsimg.com/blobby/go/daf47534-09c5-48e5-a358-420f36953360/Tea%20Trails%20Dec%202018.pdf"  
+            style="text-decoration: underline;" >
+            Kanhar Munshi</a> on Scribd
+        </p>
+        <iframe class="scribd_iframe_embed" title="122018_SunilMunshiTeaTrails" src="https://www.scribd.com/embeds/521142428/content?start_page=1&view_mode=scroll&access_key=key-ONuJvvprgq1UiS9cmiBk" data-auto-height="false" data-aspect-ratio="0.7136075949367089" scrolling="no" id="doc_51841" width="300" height="300" frameborder="1"></iframe>
         </section>
         </div>
     `;
